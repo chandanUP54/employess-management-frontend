4bcfc27fa95ace9bd5f7173c2ef26be67b4cea56
chandan <chandanmau2018@gmail.com>
2023-10-05 20:56:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import AddEmp from './Component/AddEmp';
 import EditEmp from './Component/EditEmp';
@@ -8,15 +7,12 @@ import {Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div >
-        
       <Navbar />
       <Routes>
          <Route path="/" element={<Home />}>  </Route>
          <Route path="/addEmp" element={<AddEmp />}>  </Route>
          <Route path="/editEmp/:id" element={<EditEmp />}>  </Route>
-    
       </Routes>
-      
     </div>
   );
 }
--- src/Service/emp.service.js
+++ src/Service/emp.service.js
@@ -1,27 +1,29 @@
 import axios from "axios";
 
-const BASE_API_URL = "http://localhost:8079/api/v1";
+//connected by eclipse project name -->  ems1
+
+const BASE_API_URL = "http://localhost:8091";
 
 class EmpService {
 
     saveEmp(emp) {
-        return axios.post(BASE_API_URL + "/save", emp);
+        return axios.post(BASE_API_URL + "/employees/save", emp);
     }
 
     getAllEmp() {
-        return axios.get(BASE_API_URL + "/");
+        return axios.get(BASE_API_URL + "/employees");
     }
 
     getEmpById(id) {
-        return axios.get(BASE_API_URL + "/" + id);
+        return axios.get(BASE_API_URL + "/employees/" + id);
     }
 
     deleteEmp(id) {
-        return axios.get(BASE_API_URL + "/delete/" + id);
+        return axios.delete(BASE_API_URL + "/employees/delete/" + id);
     }
 
     updateEmp(id, emp) {
-        return axios.post(BASE_API_URL + "/update/" + id, emp);
+        return axios.put(BASE_API_URL + "/employees/update/" + id, emp);
     }
 }
 
